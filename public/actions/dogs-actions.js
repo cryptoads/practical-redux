215e9062225d622b5065ff3394240e6df221d3ec
Chris Michels <chrisjohnmichels@gmail.com>
2018-06-21 20:21:42 -0400

    works

--- public/actions/dogs-actions.js
+++ public/actions/dogs-actions.js
@@ -1,1 +1,28 @@
-// put stuff here
+let dogsAction = () => {
+    // Before any search happens, we tell the reducer that we're starting a search
+    store.dispatch({
+        type: "DOG_SEARCH_INITIATED"
+    })
+
+    $.ajax({
+        method: "GET",
+        url: "https://dog.ceo/api/breeds/image/random"
+
+    }).then( (dog) => {
+        // Asynchronously dispatch another action when the movies come back from the API
+        if (dog.Error) {
+            store.dispatch({
+                type: "DOG_ERROR",
+                error: dog.status
+            });
+        } else {
+            store.dispatch({
+                type: "DOG_RETURNED",
+                dog: dog.message
+            });
+        }   
+
+        
+    } )
+    
+}
