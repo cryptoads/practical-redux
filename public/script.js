0feaec8d6dcfd61025b86b8447e24741401d9293
Chris Michels <chrisjohnmichels@gmail.com>
2018-06-21 20:40:07 -0400

    fixed loading screen

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-let store = Redux.createStore(mainReducer);
+let store = Redux.createStore(mainReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const renderApp = () => {
 	let navBar = document.getElementById("nav-bar");
--- public/views/dogs/dogs.js
+++ public/views/dogs/dogs.js
@@ -1,10 +1,18 @@
 const renderDogsApp = () => {
 	let { dogs } = store.getState();
+	let { isLoading, dog, error } = dogs;
 	console.log(dogs)
 	console.log(dogs.message)
 	return `
-
-		<img src=${dogs.dog} /><br>
+					${isLoading 
+				? `<h1> LOADING... </h1>`
+				: error
+				  ? `<h1>${error}</h1>`
+				  : `<div>
+						<img src=${dog} /><br>
+				     </div>`
+			}
+		
 		<button onclick="dogsAction()">Dog</button>
 
 
